353678e71d10c864a88569ba57a911145cc20bec
wndvlf96 <55437159+wndvlf96@users.noreply.github.com>
2023-12-11 20:51:50 +0900

    Delete IITP_ES2 directory

--- IITP_ES2/losses/loss_util.py
+++
@@ -1,8 +0,0 @@
-from .loss_IITP_ESNet import loss_IITP_ESNet, loss_IITP_ESNet_corr
-from attrdict import AttrDict
-
-def get_lossfns():
-    loss_fns = AttrDict()
-    loss_fns["loss_IITP_ESNet"] = loss_IITP_ESNet
-    loss_fns["loss_IITP_ESNet_corr"] = loss_IITP_ESNet_corr
-    return loss_fns
